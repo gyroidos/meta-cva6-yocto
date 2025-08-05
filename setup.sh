296f318d5c1de7e6e03709a4a8538fadd5dc9702
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2025-08-05 08:23:01 +0200

    setup.sh: adapt to new {bblayers|local}.conf.sample files
    Removed the generation from auto.conf since, we now have a
    local.conf.sample file with the corresponding configuration. We
    also do not need to add layers manually here anymore, since the
    layer setup is now in bblayers.conf.sample.
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>
    Tested-by: Angela Gonzalez <angela.gonzalez@planv.tech>

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 # Bootstrapper for buildbot slave
 
 DIR="build"
+SRC_DIR="$(pwd)"
 MACHINE="cv32a6-genesys2"
-CONFFILE="conf/auto.conf"
+CONFFILE="conf/local.conf"
 BITBAKEIMAGE="core-image-minimal"
 
 # make sure sstate is there
@@ -15,68 +16,20 @@ BITBAKEIMAGE="core-image-minimal"
 #umask -S u=rwx,g=rx,o=rx
 #chmod -R 755 .
 
-# Reconfigure dash on debian-like systems
-which aptitude > /dev/null 2>&1
-ret=$?
-if [ "$(readlink /bin/sh)" = "dash" -a "$ret" = "0" ]; then
-  sudo aptitude install expect -y
-  expect -c 'spawn sudo dpkg-reconfigure -freadline dash; send "n\n"; interact;'
-elif [ "${0##*/}" = "dash" ]; then
-  echo "dash as default shell is not supported"
-  return
+# fix the configuration
+if [ -e $CONFFILE ]; then
+    rm -rf $CONFFILE
 fi
+
 # bootstrap OE
-echo "Init OE"
+echo "Init OE and creating local.conf"
+export TEMPLATECONF=${SRC_DIR}/meta-cva6-yocto/conf/templates/default
 export BASH_SOURCE="openembedded-core/oe-init-build-env"
-. ./openembedded-core/oe-init-build-env $DIR
+. ./poky/oe-init-build-env $DIR
 
 # Symlink the cache
 #echo "Setup symlink for sstate"
 #ln -s ~/sstate/${MACHINE} sstate-cache
-
-# add the missing layers
-echo "Adding layers"
-bitbake-layers add-layer ../meta-openembedded/meta-oe
-bitbake-layers add-layer ../meta-openembedded/meta-python
-bitbake-layers add-layer ../meta-openembedded/meta-multimedia
-bitbake-layers add-layer ../meta-openembedded/meta-networking
-bitbake-layers add-layer ../meta-riscv
-bitbake-layers add-layer ../meta-cva6-yocto
-
-# fix the configuration
-echo "Creating auto.conf"
-
-if [ -e $CONFFILE ]; then
-    rm -rf $CONFFILE
-fi
-cat <<EOF > $CONFFILE
-MACHINE ?= "${MACHINE}"
-# rootfs for debugging
-#IMAGE_GEN_DEBUGFS = "1"
-#IMAGE_FSTYPES_DEBUGFS = "tar.gz"
-EXTRA_IMAGE_FEATURES:append = " tools-debug"
-EXTRA_IMAGE_FEATURES:append = " debug-tweaks"
-EXTRA_IMAGE_FEATURES:append = " ssh-server-dropbear"
-EXTRA_IMAGE_FEATURES:append = " package-management"
-PACKAGECONFIG:append:pn-qemu-native = " sdl"
-PACKAGECONFIG:append:pn-nativesdk-qemu = " sdl"
-USER_CLASSES ?= "buildstats buildhistory buildstats-summary image-prelink"
-
-require conf/distro/include/no-static-libs.inc
-require conf/distro/include/yocto-uninative.inc
-require conf/distro/include/security_flags.inc
-
-INHERIT += "uninative"
-
-DISTRO_FEATURES:append = " largefile multiarch pam "
-#DISTRO_FEATURES_BACKFILL_CONSIDERED += "sysvinit"
-#VIRTUAL-RUNTIME_init_manager = "systemd"
-INIT_MANAGER = "mdev-busybox"
-HOSTTOOLS_NONFATAL:append = " ssh"
-SYSVINIT_ENABLED_GETTYS = ""
-IMAGE_INSTALL:append = " vitetris"
-IMAGE_INSTALL:append = " pacman4console"
-EOF
 
 echo "To build an image run"
 echo "---------------------------------------------------"
